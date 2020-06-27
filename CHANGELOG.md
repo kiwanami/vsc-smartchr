25b73ff0c42637460da5b899467c3c1c7aa27b36
SAKURAI Masashi <m_sakurai@si-seiko.com>
2020-06-27 18:02:15 +0900

    fixed: canceling by changing cursor position

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to the "smartchr" extension will be documented in this file.
 
 ## [Unreleased]
 
+## 0.1.1 - 2020-06-27
+
+- Fixed canceling smartchr expansition by changing cursor position 
+
 ## 0.1.0 - 2020-04-19
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -22,17 +22,21 @@ export function activate(context: vscode.ExtensionContext) {
 		controller.cancel();
 	});
 
+	vscode.window.onDidChangeTextEditorSelection(event => {
+		controller.moveCursor(event.textEditor, event.selections);
+	});
+
 	vscode.workspace.onDidChangeConfiguration(event => {
-        let affected = event.affectsConfiguration("smartchr.definitions");
-        if (affected) {
-            controller.reload();
-        }
-    });
+		let affected = event.affectsConfiguration("smartchr.definitions");
+		if (affected) {
+			controller.reload();
+		}
+	});
 }
 
 export type SCEditBuilder = {
 	delete: (range: vscode.Range) => void,
-	insert: (range: vscode.Position, text:string) => void,
+	insert: (range: vscode.Position, text: string) => void,
 };
 
 export type SCEditBuilderBlock = (builder: SCEditBuilder) => void;
@@ -51,6 +55,9 @@ export class SmartchrController {
 	private lastDef: SmartchrDef | null = null;
 	private lastEditor: SCEditor | null = null;
 	private count: number = 0;
+	private lastChar: number | null = null;
+	private lastLine: number | null = null;
+
 
 	private getLangID = () => {
 		return vscode.window.activeTextEditor?.document.languageId;
@@ -59,23 +66,23 @@ export class SmartchrController {
 		vscode.commands.executeCommand('default:type', { text: text });
 	};
 
-	constructor(testConfig:any = undefined) {
+	constructor(testConfig: any = undefined) {
 		if (testConfig) {
 			this.loadConfig(testConfig.langs);
 			this.getLangID = () => testConfig.langID;
-			this.processDefault = (text:string) => testConfig.processDefault(text);
+			this.processDefault = (text: string) => testConfig.processDefault(text);
 		} else {
 			this.reload();
 		}
 	}
 
-	private loadConfig(defConfig:any) {
+	private loadConfig(defConfig: any) {
 		for (const id of Object.keys(defConfig)) {
 			let keymap = new Map<string, SmartchrDef[]>();
 			this.keyMapDefs.set(id, keymap);
 			let langDefs = defConfig[id];
 			for (const k of Object.keys(langDefs)) {
-				let defs = langDefs[k].map((p:string) => parsePattern(p));
+				let defs = langDefs[k].map((p: string) => parsePattern(p));
 				keymap.set(k, defs);
 			}
 		}
@@ -83,11 +90,23 @@ export class SmartchrController {
 
 	reload(): void {
 		let smartchrConfig = vscode.workspace.getConfiguration("smartchr");
-		let defConfig:any = smartchrConfig.get("definitions");
+		let defConfig: any = smartchrConfig.get("definitions");
 		if (!defConfig) {
 			return;
 		}
 		this.loadConfig(defConfig);
+	}
+
+	moveCursor(textEditor: vscode.TextEditor, selections: readonly vscode.Selection[]): void {
+		if (this.lastChar === null && this.lastLine === null) {
+			// do nothing just during smartchr expansion task
+			return;
+		}
+		if (selections[0].active.character !== this.lastChar ||
+			selections[0].active.line !== this.lastLine) {
+			// determine changing cursor position after smartchr expansion
+			this.cancel();
+		}
 	}
 
 	public process(key: string, editor: SCEditor): Thenable<any> {
@@ -95,7 +114,7 @@ export class SmartchrController {
 		let defs = null;
 		if (langid) {
 			let keymap = this.keyMapDefs.get(langid);
-			if (keymap)	{defs = keymap.get(key);}
+			if (keymap) { defs = keymap.get(key); }
 		}
 		if (!defs) {
 			this.cancel();
@@ -109,7 +128,7 @@ export class SmartchrController {
 		} else {
 			this.count = 0;
 		}
-		if (this.count >= defs.length) {this.count = 0;}
+		if (this.count >= defs.length) { this.count = 0; }
 		let def = defs[this.count];
 		let cursorDiff: number | void;
 		return editor.edit((builder) => {
@@ -120,12 +139,16 @@ export class SmartchrController {
 			this.lastKey = key;
 			this.lastDef = def;
 			cursorDiff = def.insert(editor.selection.active, builder);
+			this.lastChar = null; // wait for fixing cursor position
+			this.lastLine = null; 
 		}).then(() => {
+			let cpos = editor.selection.active;
 			if (cursorDiff) {
-				let cpos = editor.selection.active;
 				let npos = new vscode.Position(cpos.line, cpos.character + cursorDiff);
 				editor.selection = new vscode.Selection(npos, npos);
 			}
+			this.lastChar = editor.selection.active.character;
+			this.lastLine = editor.selection.active.line;
 		});
 	}
 
